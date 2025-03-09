d36c914c17d58ac552d757efec6b24d3c70e87b0
= <=>
2025-03-09 22:08:29 +0300

    first commit

---
+++ config/plugins.ts
@@ -0,0 +1,55 @@
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          region: env("AWS_REGION"),
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+          },
+          params: {
+            ACL: "public-read",
+            Bucket: env("AWS_BUCKET_NAME"),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: false,
+      landingPage: false, // playgroundAlways
+      depthLimit: 8,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.gmail.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: "hello@example.com",
+        defaultReplyTo: "hello@example.com",
+      },
+    },
+  },
+});
